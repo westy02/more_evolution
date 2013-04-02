64c9c6b68110f66f607b2224acb5d9433eda9352
westy02 <westy02@gmail.com>
2013-04-02 21:03:30 +1300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoreEvolution::Application.config.secret_token = '64771025bd4e293b46acb3daee197ea92bc625633c33be6c4ca2e778078633462e6f641270fa54249130693ca1e438dd0ba3397597ca1b9af9a0962dc765e1f7'
